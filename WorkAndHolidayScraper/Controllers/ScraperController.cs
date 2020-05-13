7a42dbeba27b0c01757286ea436b20a0946f3d8a
albero94 <ealbero94@gmail.com>
2020-05-13 19:36:25 -0400

    Add project files.

---
+++ WorkAndHolidayScraper/Controllers/ScraperController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WorkAndHolidayScraper.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScraperController : Controller
+    {
+        public string url = "https://www.workingholidayjobs.com.au/jobs/";
+        public ActionResult Index()
+        {
+            var startTime = DateTime.Now;
+
+            Thread.Sleep(1000);
+
+            var totalTime = DateTime.Now - startTime;
+            return Ok($"You are done! Time spent: {totalTime.Duration()})");
+        }
+    }
+}
