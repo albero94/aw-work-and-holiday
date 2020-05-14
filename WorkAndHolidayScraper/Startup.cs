245d1ac2d3d30c57c7cc49ea1bd1b0ac0b84e748
albero94 <ealbero94@gmail.com>
2020-05-13 20:27:48 -0400

    chore: Logger configuration

--- WorkAndHolidayScraper/Startup.cs
+++ WorkAndHolidayScraper/Startup.cs
@@ -26,6 +26,7 @@ namespace WorkAndHolidayScraper
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddLogging();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
