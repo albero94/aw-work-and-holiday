f1c4632c50f8a093a6889d53da414c014c401b2c
albero94 <ealbero94@gmail.com>
2020-05-13 20:27:23 -0400

    feat: Create model for job entry rows
    This is the list in the main page

---
+++ WorkAndHolidayScraper/Models/JobRowEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkAndHolidayScraper.Models
+{
+    public class JobRowEntry
+    {
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public string Location { get; set; }
+        public string Date { get; set; }
+        public string Type { get; set; }
+    }
+}
