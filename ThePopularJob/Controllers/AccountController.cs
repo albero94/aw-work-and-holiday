ede63fface4d2df70d45b8a4ae8e67c9573a80b2
albero94 <ealbero94@gmail.com>
2020-06-12 09:26:11 -0400

    feat: Add account controller and actions
    Still Identity services have not been registered properly

---
+++ ThePopularJob/Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using ThePopularJob.ViewModels;
+
+namespace ThePopularJob.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly ILogger<AccountController> logger;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            ILogger<AccountController> logger)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.Username, Email = model.Email };
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    // send confirmation link
+                    RedirectToRegistrationMessageView();
+                }
+                PrintModelErrors(result.Errors);
+            }
+            return View();
+        }
+
+
+        // PRIVATE METHODS
+        private void PrintModelErrors(IEnumerable<IdentityError> errors)
+        {
+            foreach (var error in errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
+        private IActionResult RedirectToRegistrationMessageView()
+        {
+            ViewBag.ErrorTitle = "Registration successful";
+            ViewBag.ErrorMessage = "Before you can login, please confirm your email by clicking on the confirmatoin link we have sent you";
+            return View("Error");
+        }
+    }
+}
