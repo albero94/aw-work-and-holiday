2216021910e4a69589dca08ff949457222a036f9
albero94 <ealbero94@gmail.com>
2020-06-15 20:49:15 -0400

    feat: Add login page

--- ThePopularJob/Controllers/AccountController.cs
+++ ThePopularJob/Controllers/AccountController.cs
@@ -24,6 +24,28 @@ namespace ThePopularJob.Controllers
             this.logger = logger;
         }
 
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, lockoutOnFailure: false);
+
+                if (result.Succeeded) 
+                    return RedirectToAction("Index", "Home");
+
+                ModelState.AddModelError("", "Invalid Login Attempt");
+            }
+            return View();
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
