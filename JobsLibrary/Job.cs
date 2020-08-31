7ed4e3c3ce9146846dfcfa9870fcb8ab8dfab910
albero94 <ealbero94@gmail.com>
2020-08-31 08:36:35 -0400

    JobsLibrary - feat: Add some fields to job class

--- JobsLibrary/Job.cs
+++ JobsLibrary/Job.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -20,17 +21,24 @@ namespace JobsLibrary
         [Column("location")]
         [Required]
         public string Location { get; set; }
+        [Column("city")]
+        public string City { get; set; }
+        [Column("state")]
+        public string State { get; set; }
         [Column("date")]
         public DateTime? Date { get; set; }
         [Column("description")]
         [Required]
         public string Description { get; set; }
+        [Column("long_description")]
+        public string LongDescription { get; set; }
         [Column("href")]
         public string? Href { get; set; }
         [Column("original_website")]
         public string? OriginalWebsite { get; set; }
         [Column("salary")]
         public string? Salary { get; set; }
+        public List<string> Categories { get; set; }
         public IdentityUser? User { get; set; }
     }
 }
